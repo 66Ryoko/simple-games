5c48d3aee1f1a3602037f7363f192fe633a0a52c
Ryoko <ryoko.li@skywatch.com.tw>
2024-05-31 15:13:01 +0800

    refine: refine nav bar style

--- app/ui/nav-links.tsx
+++ app/ui/nav-links.tsx
@@ -14,13 +14,17 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className={clsx('rounded-full  px-3 py-2 text-sm font-semibold ', {
-              'bg-sky-50 text-sky-500 hover:bg-sky-100': pathname === link.href,
-              'bg-white text-gray-500 hover:bg-gray-100':
-                pathname !== link.href,
-            })}
+            className={clsx(
+              'hidden rounded-full px-3  py-2 text-sm font-semibold md:block ',
+              {
+                'bg-sky-50 text-sky-500 hover:bg-sky-100':
+                  pathname === link.href,
+                'bg-white text-gray-500 hover:bg-gray-100':
+                  pathname !== link.href,
+              },
+            )}
           >
-            <p className="hidden md:block">{link.name}</p>
+            <p>{link.name}</p>
           </Link>
         );
       })}
--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -2,7 +2,7 @@ import NavLinks from '@/app/ui/nav-links';
 import NavLogo from '@/app/ui/nav-logo';
 export default function NavBar() {
   return (
-    <div className="align-center sticky top-0 flex h-16 w-full content-center items-center justify-between border-b border-gray-200 bg-white px-12 py-2 shadow-lg shadow-gray-100">
+    <div className="align-center sticky top-0 z-40 flex h-16 w-full content-center items-center justify-between border-b border-gray-200 bg-white px-4 py-2 shadow-lg shadow-gray-100 md:px-12">
       <NavLogo />
       <NavLinks />
     </div>
