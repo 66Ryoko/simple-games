d63210afa8374238b252459e253824256aa49bd5
Ryoko <ryoko.li@skywatch.com.tw>
2024-06-14 16:10:02 +0800

    refine: reduce unnecessary calculations

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -68,7 +68,7 @@ export function calculateWinner(
   currentMoveIndex: number,
   totalMoves: number,
 ): { gameOver: boolean; winner: SquareValue } {
-  if (totalMoves < boardSize) {
+  if (totalMoves < boardSize * 2) {
     return { gameOver: false, winner: null };
   }
   const row: number = currentMoveIndex % boardSize;
